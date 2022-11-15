77c3af8911f889b6710f601325300925cf440d58
LouisLapointe01 <114722726+LouisLapointe01@users.noreply.github.com>
2022-11-15 09:41:37 +0100

    TP4
    Jusqu'à 2.3 Ronin

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -3,19 +3,19 @@ import personnages.*;
 public class HistoireTP4 {
 
 	public static void main(String[] args) {
-		System.out.println("\n\nTEST Humain : \n");
+		System.out.println("\n\nTEST Humain : \n"); /*    Humain    */
 		Humain humain = new Humain("Prof","kombucha",54);
 		humain.direBonjour();
 		humain.acheter("boisson", 12);
 		humain.boire();
 		humain.acheter("kimono", 50);
-		System.out.println("\n\nTEST Commercant : \n");
+		System.out.println("\n\nTEST Commercant : \n"); /*    COMMERCANT    */
 		Commercant commercant = new Commercant("Carefulu",100);
 		commercant.direBonjour();
 		commercant.seFaireExtorquer();
 		commercant.recevoireArgent(15);
 		commercant.boire();
-		System.out.println("\n\nTEST Yakuza : \n");
+		System.out.println("\n\nTEST Yakuza : \n"); /*    YAKUZA    */
 		Yakuza yakuza = new Yakuza("Harukichi",120,"Whisky","Yamaguchi",50);
 		yakuza.direBonjour();
 		yakuza.parler("Tiens, tiens ne sarait-il pas un fiable marchant qui passe par là ?");
